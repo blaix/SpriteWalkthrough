2bb42c415699c752a51abca234ca7880c59c7e93
Justin Blake <justin@hentzia.com>
2014-04-12 06:04:58 -0400

    Add empty scene

--- app/controllers/view_controller.rb
+++ app/controllers/view_controller.rb
@@ -11,4 +11,10 @@ class ViewController < UIViewController
     spriteView.showsNodeCount = true
     spriteView.showsFPS = true
   end
+
+  def viewWillAppear(animated)
+    hello = HelloScene.alloc.initWithSize(CGSizeMake(768, 1024))
+    spriteView = self.view
+    spriteView.presentScene(hello)
+  end
 end
---
+++ app/scenes/hello_scene.rb
@@ -0,0 +1,3 @@
+class HelloScene < SKScene
+  # hello!
+end
