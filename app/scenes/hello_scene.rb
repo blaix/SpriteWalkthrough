de6585b02c64215e3643c07dbfcea605d229383b
Justin Blake <justin@hentzia.com>
2014-04-14 21:22:45 -0400

    Add content to the scene
    Adds "Hello, World!" over a blue background.
    
    https://developer.apple.com/library/ios/documentation/GraphicsAnimation/Conceptual/SpriteKit_PG/GettingStarted/GettingStarted.html#//apple_ref/doc/uid/TP40013043-CH2-SW6

--- app/scenes/hello_scene.rb
+++ app/scenes/hello_scene.rb
@@ -1,3 +1,24 @@
 class HelloScene < SKScene
-  # hello!
+  attr_accessor :contentCreated
+
+  def didMoveToView(view)
+    return if self.contentCreated
+    self.createSceneContents
+    self.contentCreated = true
+  end
+
+  def createSceneContents
+    self.backgroundColor = UIColor.blueColor # how to use SKColor macro in RM?
+    self.scaleMode = SKSceneScaleModeAspectFit
+    self.addChild(self.newHelloNode)
+  end
+
+  def newHelloNode
+    helloNode = SKLabelNode.labelNodeWithFontNamed("Chalkduster")
+    helloNode.text = "Hello, World!"
+    helloNode.fontSize = 42
+    helloNode.position = CGPointMake(CGRectGetMidX(self.frame),
+                                     CGRectGetMidY(self.frame))
+    helloNode
+  end
 end
